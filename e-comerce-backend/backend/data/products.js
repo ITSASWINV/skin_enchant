24ccf44f3f577271767088230c6e283e28b3434b
ITSASWINV <aswinvezhakkodan73@gmail.com>
2023-06-25 13:40:25 +0530

    first commit

---
+++ e-comerce-backend/backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Natural Baby Soap",
+    imageUrl: 'https://files.themomsco.com/site-images/800x800/FOP_39.jpg',
+    
+    description:
+    "Our natural baby soap is a gentle and nourishing formula, carefully crafted with pure ingredients to provide a soothing and safe bathing experience for your little one's delicate skin.",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "Natural Sunscreen",
+    imageUrl:
+      "https://files.themomsco.com/site-images/800x800/FOP-WITH-INGREDIENTS-(1).jpg",
+    description:
+      "Natural sunscreen is a sun protection product formulated with mineral-based ingredients like zinc oxide or titanium dioxide. It provides broad-spectrum protection against harmful UV rays without the use of synthetic chemicals or additives.",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "Natural Body Wash",
+    imageUrl:
+      "https://files.stbotanica.com/site-images/800x800/d338f200-1fbc-11ec-83b7-c7f6905fb422.jpg",
+    description:
+      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "Natural Mosquito repellent",
+    imageUrl:
+      "https://images.mamaearth.in/catalog/product/m/g/mg_6805_probs_kx4asg2xuppec3db.jpg?fit=contain&width=332",
+    description:
+      "Natural mosquito repellent is a non-toxic insect repellent made from plant-derived ingredients such as essential oils (e.g., citronella, lemongrass, eucalyptus) that effectively repel mosquitoes and other biting insects. It provides protection without the use of synthetic chemicals like DEET.",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "Natural Under Eye Cream",
+    imageUrl:
+      "https://www.o3plus.com/pub/media/catalog/product/cache/dd0499f648c07c35a3a4972369e82147/p/r/product_copy_1_10.jpg",
+    description:
+      "A natural under eye cream is a soothing and nourishing skincare product specifically designed to target dark circles, puffiness, and fine lines around the delicate under eye area using botanical extracts and oils.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "Natural Face Toner",
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0023/9953/5204/products/Skin-Brightening-Listing-20220118-1.jpg?v=1655701668&width=800",
+    description:
+      "A natural face toner is a gentle skincare product made from plant-based ingredients that helps balance the skin's pH, tighten pores, and refresh the complexion without harsh chemicals or artificial additives.",
+    price: 140,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
